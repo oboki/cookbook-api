e57b14afde68ef30f1fc9fea368ebe0816b122a0
oboki <57709910+oboki@users.noreply.github.com>
2021-09-13 12:06:20 +0900

    1.0.0 (#1)
    * 210819
    
    * 210823
    
    * 201824
    
    * 210826
    
    * 210830
    
    * 210831
    
    * 210901
    
    * 210906
    
    * 210912

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,28 @@
-import logging
-import json
-from urllib.parse import unquote
+from .model import (
+    BaseDocumentModel, BaseSearchResultModel, MatchAllSearchResultModel,
+    SearchByAuthorResultModel, SearchByColumnNameResultModel,
+    SearchByParentIdResultModel, WildcardSearchResultModel
+)
 
 from flask import (
-    Markup, Response, escape, flash, jsonify, make_response, redirect, render_template, request,
-    session as flask_session, url_for, g,
+    jsonify, request,
+    session as flask_session, g,
 )
-from flask_appbuilder import BaseView, ModelView, expose, has_access, permission_name
+from flask_appbuilder import expose, has_access, permission_name
 from flask_appbuilder import BaseView as AppBuilderBaseView
+from flask_appbuilder.security.decorators import has_access_api
 
-import airflow
-from airflow.exceptions import AirflowException
 from airflow.utils.db import provide_session
 from airflow.www_rbac import utils as wwwutils
-from airflow.www_rbac.app import app, appbuilder
-from airflow.www_rbac.decorators import action_logging, gzipped, has_dag_access
+from airflow.www_rbac.app import csrf
 from airflow.plugins_manager import AirflowPlugin
 
-from pathlib import Path
-import sys
-sys.path.insert(0, str(Path(__file__).parent.absolute()))
-
-from model import BaseDetailModel, BaseSearchResultModel
-
+from urllib.parse import unquote
+from copy import deepcopy
+import json
+import logging
 logger = logging.getLogger("cookbook-api")
+
 
 class CookbookApi(AppBuilderBaseView):
 
@@ -34,26 +33,146 @@ class CookbookApi(AppBuilderBaseView):
     def index(self, session=None):
         return ''
 
-    #@has_access
-    #@permission_name("list")
+    # @has_access_api
+    # @permission_name("list")
     @provide_session
-    @expose('/v1/<index>/search')
-    def search(self, index, session=None):
-        query = unquote(request.args.get('s', ''))
-        if not query:
-            return wwwutils.json_response([])
-
-        search = BaseSearchResultModel(query, index)
+    @expose('/v1/<index>')
+    def match_all(self, index, session=None):
+        search = MatchAllSearchResultModel(index)
         return wwwutils.json_response(search.get_result())
 
+    # @has_access_api
+    # @permission_name("list")
 
-    #@has_access
-    #@permission_name("list")
+    @provide_session
+    @csrf.exempt
+    @expose('/v1/<index>/search', methods=['GET', 'POST'])
+    def search(self, index, session=None):
+        size = int(unquote(request.args.get('size', '4')))
+        page = int(unquote(request.args.get('page', '0')))
+
+        payload = None
+        if request.method == 'POST':
+            payload = deepcopy(request.get_json()['data'])
+
+        query = unquote(request.args.get('s', ''))
+        if not query:
+            search = MatchAllSearchResultModel(
+                index,
+                size=size,
+                offset=page*size,
+                advanced=payload
+            )
+            return wwwutils.json_response(search.get_result())
+
+        wildcard = unquote(request.args.get('wildcard', ''))
+        if wildcard:
+            search = WildcardSearchResultModel(query, index)
+            return wwwutils.json_response(search.get_result())
+
+        by_parent_id = unquote(request.args.get('by-parent-id', ''))
+        if by_parent_id:
+            search = SearchByParentIdResultModel(
+                query,  # parent_id
+                index
+            )
+            return wwwutils.json_response(search.get_result())
+
+        by_column_name = unquote(request.args.get('by-column-name', ''))
+        if by_column_name:
+            search = SearchByColumnNameResultModel(
+                query,  # column_name
+                index
+            )
+            return wwwutils.json_response(search.get_result())
+
+        by_author = unquote(request.args.get('by-author', ''))
+        if by_author:
+            search = SearchByAuthorResultModel(
+                query,
+                index,
+                size=size,
+                offset=page*size
+            )
+            return wwwutils.json_response(search.get_result())
+
+        search = BaseSearchResultModel(
+            query,
+            index,
+            size=size,
+            offset=page*size,
+            advanced=payload
+        )
+        return wwwutils.json_response(search.get_result())
+
+    # @has_access
+    # @permission_name("list")
+
     @provide_session
     @expose('/v1/<index>/<id>')
     def detail(self, index, id, session=None):
-        detail = BaseDetailModel(index, id)
+        detail = BaseDocumentModel(index, id)
         return wwwutils.json_response(detail.show())
+
+    # @has_access
+    # @permission_name("list")
+
+    @provide_session
+    @expose('/v1/whoami')
+    def get_current_user(self, session=None):
+        if g and hasattr(g, 'user') and g.user:
+            try:
+                username = g.user.username
+            except AttributeError:
+                username = 'Anonymous'
+
+        return wwwutils.json_response({'username': username})
+
+    # @has_access
+    # @permission_name("edit")
+
+    @provide_session
+    @csrf.exempt
+    @expose('/v1/<index>/add', methods=['POST'])
+    def create(self, index, session=None):
+        req = request.get_json()
+
+        doc = BaseDocumentModel(index, doc=req['data'])
+        doc.create()
+
+        return jsonify({
+            'created_doc_id': doc.id
+        })
+
+    # @has_access
+    # @permission_name("edit")
+
+    @provide_session
+    @csrf.exempt
+    @expose('/v1/<index>/edit/<id>', methods=['POST'])
+    def edit(self, index, id, session=None):
+        doc = request.get_json()['data']
+
+        document = BaseDocumentModel(index, id=id)
+        document.update(kwargs=doc)
+
+        return jsonify({
+            'status': 'success'
+        })
+
+    # @has_access
+    # @permission_name("edit")
+
+    @provide_session
+    @csrf.exempt
+    @expose('/v1/<index>/delete/<id>', methods=['POST'])
+    def delete(self, index, id, session=None):
+        document = BaseDocumentModel(index, id=id)
+        document.delete()
+
+        return jsonify({
+            'status': 'success'
+        })
 
 
 v_appbuilder_view = CookbookApi()
--- app/model.py
+++ app/model.py
@@ -1,37 +1,85 @@
+import logging
 from elasticsearch import Elasticsearch
+from hashlib import blake2b
+from datetime import datetime
+from copy import deepcopy
+import json
+
+
+def create_hash_id(s):
+    h = blake2b(digest_size=10)
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+
+
+def current_ts_isof():
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+
+logger = logging.getLogger("cookbook-api")
+
 es_client = Elasticsearch(
     'localhost:9200',
     timeout=10
 )
 
-class BaseDetailModel:
+
+class BaseDocumentModel:
 
     def __init__(
         self,
         index,
-        id=None
+        id=None,
+        **kwargs
     ):
-        self.id = id
+        self.current_ts = current_ts_isof()
         self.index = index
-        self.source = es_client.search(
-            index=index,
-            body={
-                "query": {
-                    "ids": {
-                        "values": [self.id]
+        self.kwrags = kwargs
+
+        if id:
+            self.id = id
+            self.source = es_client.search(
+                index=index,
+                body={
+                    "query": {
+                        "ids": {
+                            "values": [id]
+                        }
                     }
                 }
-            }
-        )['hits']['hits'][0]['_source']
+            )['hits']['hits'][0]['_source']
+
+        else:
+            self.doc = kwargs['doc']
+            self.doc.update({
+                'created_ts': self.current_ts,
+                'modified_ts': self.current_ts
+            })
+            if index == 'codes':
+                self.id = create_hash_id(''.join([
+                    kwargs['doc']['column_name'],
+                    kwargs['doc']['code']
+                ]))
+            elif index == 'comments':
+                self.id = create_hash_id(''.join([
+                    kwargs['doc']['author'],
+                    self.current_ts
+                ]))
 
     def show(self, **kwargs):
         return self.source
 
-    def update(self, **doc):
+    def update(self, **kwargs):
         es_client.update(
             index=self.index,
             id=self.id,
-            body=doc
+            body={'doc': kwargs['kwargs']}
+        )
+
+    def delete(self, **kwargs):
+        es_client.delete(
+            index=self.index,
+            id=self.id
         )
 
     def refresh(self):
@@ -39,8 +87,14 @@ class BaseDetailModel:
             index=self.index
         )
 
-    def create(self, **kwargs):
-        pass
+    def create(self):
+        es_client.index(
+            index=self.index,
+            id=self.id,
+            doc_type='_doc',
+            body=self.doc,
+        )
+        self.refresh()
 
 
 class BaseSearchResultModel:
@@ -49,41 +103,204 @@ class BaseSearchResultModel:
         self,
         query,
         index,
-        size=5,
-        offset=0
+        size=4,
+        offset=0,
+        advanced=None
     ):
-        self.query = query
         self.index = index
-
-        body = {
+        self.body = {
             "size": size,
             "from": offset,
             "query": {
-                "multi_match": {
-                    "query": query,
-                    "fields": {
-                        "tables": ['table_name', 'entity_name', 'description'],
-                        "columns": ['column_name', 'attribute_name', 'description'],
-                        "codes": ['code', 'description'],
-                        "comments": ['comment']
-                    }[index]
+                "bool": {
+                    "must": [
+                        {
+                            "multi_match": {
+                                "query": query,
+                                "fields": {
+                                    "tables": ['table_name', 'entity_name', 'description'],
+                                    "columns": ['column_name', 'attribute_name', 'description'],
+                                    "codes": ['code', 'description'],
+                                    "comments": ['comment'],
+                                    "autocomplete_keywords": ['keyword']
+                                }[index]
+                            }
+                        }
+                    ]
                 }
-            },
+            }
+        }
+
+        self.advanced = advanced
+        if self.advanced:
+            self.update_body_with_advanced_query()
+
+        logger.info(json.dumps(self.body, indent=2))
+
+    def update_body_with_advanced_query(self):
+        for item in self.advanced:
+            if item['type'] == 'filter':
+                tmp = [
+                    {
+                        "term": {
+                            item['field']: item['value']
+                        }
+                    }
+                ]
+                self.body['query']['bool'].update({'filter': tmp})
+
+            if item['type'] == 'wildcard':
+                tmp = {
+                    "wildcard": {
+                        "".join([item['field'], '.keyword']): {
+                            "value": item['value']
+                        }
+                    }
+                }
+                self.body['query']['bool']['must'].append(tmp)
+
+            if item['type'] == 'exclude':
+                tmp = deepcopy(self.body['query']['bool']['must'][0])
+                tmp = {
+                    "multi_match": {
+                        "query": item['value'],
+                        "fields": {
+                            "tables": ['table_name', 'entity_name', 'description'],
+                            "columns": ['column_name', 'attribute_name', 'description'],
+                            "codes": ['code', 'description'],
+                            "comments": ['comment'],
+                            "autocomplete_keywords": ['keyword']
+                        }[self.index]
+                    }
+                }
+                logger.info(tmp)
+                self.body['query']['bool'].update({'must_not': tmp})
+
+    def get_result(self):
+        return es_client.search(
+            index=self.index,
+            body=self.body
+        )['hits']
+
+
+class MatchAllSearchResultModel(BaseSearchResultModel):
+
+    def __init__(
+        self,
+        index,
+        size=10,
+        offset=0,
+        advanced=None
+    ):
+        self.index = index
+        self.body = {
+            "size": size,
+            "from": offset,
+            "query": {"bool": {"must": [{
+                "match_all": {}
+            }]}},
             "sort": [
-                {"_score": {"order": "desc"}}
+                {"created_ts": {"order": "desc"}}
             ]
         }
 
-        self.result = es_client.search(
-            index=index,
-            body=body
-        )['hits']['hits']
+        self.advanced = advanced
+        if self.advanced:
+            self.update_body_with_advanced_query()
 
-    def get_result(self, **kwargs):
-        return self.result
+        logger.info(json.dumps(self.body, indent=2))
 
 
-class ExactSearchResultModel(BaseSearchResultModel):
+class WildcardSearchResultModel(BaseSearchResultModel):
+
+    def __init__(
+        self,
+        query,
+        index,
+        size=10,
+        offset=0
+    ):
+        self.index = index
+        self.body = {
+            "size": size,
+            "from": offset,
+            "query": {"wildcard": {
+                "keyword.keyword": {"value": "".join(['*', query, '*'])}
+            }}
+        }
+
+
+class SearchByParentIdResultModel(BaseSearchResultModel):
+
+    def __init__(
+        self,
+        query,
+        index,
+        size=1000,
+        offset=0
+    ):
+        self.index = index
+        self.body = {
+            "size": size,
+            "from": offset,
+            "query": {
+                "term": {"parent_id": {"value": query}}
+            },
+            "sort": [{{
+                "columns": "position",
+                "codes": "code.keyword",
+                "comments": "created_ts",
+                "tables": "created_ts",
+            }[index]: {"order": "asc"}}]
+        }
+
+
+class SearchByColumnNameResultModel(BaseSearchResultModel):
+
+    def __init__(
+        self,
+        query,
+        index,
+        size=1000,
+        offset=0
+    ):
+        self.index = index
+        self.body = {
+            "size": size,
+            "from": offset,
+            "query": {
+                "term": {"column_name.keyword": {"value": query}}
+            },
+            "sort": [{{
+                "columns": "position",
+                "codes": "code.keyword",
+                "comments": "created_ts",
+                "tables": "created_ts",
+            }[index]: {"order": "asc"}}]
+        }
+
+
+class SearchByAuthorResultModel(BaseSearchResultModel):
+
+    def __init__(
+        self,
+        query,
+        index,
+        size=10,
+        offset=0
+    ):
+        self.index = index
+        self.body = {
+            "size": size,
+            "from": offset,
+            "query": {
+                "term": {"author": {"value": query}}
+            },
+            "sort": [{"created_ts": {"order": "desc"}}]
+        }
+
+
+class ExactSearchResultForTableModel(BaseSearchResultModel):
     pass
 
 
